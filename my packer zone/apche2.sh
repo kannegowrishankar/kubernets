42b706ebbd90a83ac88ca9a1da62968e3915c172
unknown <kgshankar97@gmail.com>
2019-10-01 21:22:39 +0530

    practice file

---
+++ my packer zone/apche2.sh
@@ -0,0 +1,4 @@
+echo "updating packages"
+sudo apt-get update
+echo "installing apache2"
+sudo apt-get install apche2 -y
---
+++ my packer zone/nginx.sh
@@ -0,0 +1,4 @@
+echo "updating packages"
+sudo apt-get update
+echo "installing nginx"
+sudo apt-get install nginx -y
